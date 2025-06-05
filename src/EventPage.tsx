3ab18a5b668f879c4ba063b7ba5dc1c55dc9e7f2
Paul Milla <paul.milla1@gmail.com>
2025-06-04 19:39:56 -0600

    Init basic modal popup

--- src/EventPage.tsx
+++ src/EventPage.tsx
@@ -9,6 +9,19 @@ import NavigationBar from "./NavigationBar";
 import { map } from 'rxjs'
 import * as api from "./lib/inCahootsApi";
 import { useAuth } from "./auth/FirebaseAuthContext";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 type CategorizedAttendees = {
   hosts: Attendee[];
@@ -169,16 +182,39 @@ export default function EventPage() {
   
   function InviteContactsButton() {
     return (
-      <button
-        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-linear-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-hidden focus:ring-cyan-200 dark:focus:ring-cyan-800"
-        onClick={() => alert("TODO: Popup invite modal")}
-        type="button"
-      >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent dark:group-hover:bg-transparent">
-          Invite Contacts
-        </span>
-      </button>
-    )
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline">Invite Contacts</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  )
   }
 
   function RSVPButtonDropdown() {
