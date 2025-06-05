fd9814ec0d359958e7c85b98e8308c60bdedcc6a
Paul Milla <paul.milla1@gmail.com>
2025-06-04 18:28:22 -0600

    Add new button for invite contacts

--- src/EventPage.tsx
+++ src/EventPage.tsx
@@ -166,6 +166,20 @@ export default function EventPage() {
   const mapsLink = eventDetails?.location.address
     ? createGoogleMapsLink(eventDetails?.location.address)
     : null;
+  
+  function InviteContactsButton() {
+    return (
+      <button
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+        onClick={() => alert("TODO: Popup invite modal")}
+        type="button"
+      >
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+          Invite Contacts
+        </span>
+      </button>
+    )
+  }
 
   function RSVPButtonDropdown() {
     // TODO hook up functionality to change RSVP state
@@ -249,7 +263,10 @@ export default function EventPage() {
               <h1 className="text-5xl font-medium tracking-tight">
                 {eventDetails?.name}
               </h1>
-              <RSVPButtonDropdown />
+              <div className="flex justify-right">
+                <InviteContactsButton />
+                <RSVPButtonDropdown />
+              </div>
             </div>
             <div className="mt-5 grid grid-cols-1 md:grid-cols-3 gap-4">
               {/* details card */}
